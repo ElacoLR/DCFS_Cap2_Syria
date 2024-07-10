0778477e55ba1c7d1ea47aaf0d0fdef5f4522b85
Rising_Star <alex010204@gmail.com>
2024-07-11 01:36:00 +0900

    add gitignore, add templates..

--- Scripts/System/Templates.lua
+++ Scripts/System/Templates.lua
@@ -13,14 +13,17 @@ CAP.Template.Ground.Syria = {"Template_Syria_Armored", "Template_Syria_Mechanize
 
 CAP.Zones = {}
 
-CAP.Zones.Airport = {"Airport_Minakh", "Airport_Hatay", "Airport_Gaziantep", "Airport_Sanliurfa"}
-CAP.Zones.Airbase = {}
+CAP.Zones.Airport = {"Airport_Minakh", "Airport_Hatay", "Airport_Gaziantep", "Airport_Sanliurfa", "Airport_Aleppo"}
+CAP.Zones.Airbase = {"Airbase_Incirlik", "Airbase_Kuweires"}
 
 CAP.Zones.MilitaryBase = {"MilitaryBase_BA53", "MilitaryBase_BA89", "MilitaryBase_DA88"}
 
-CAP.Zones.Factory = {"Factory_CB50"}
+CAP.Zones.Factory = {"Factory_CB50", "Factory_CA04"}
+
+CAP.Zones.Outpost = {"Outpost_DB21"}
 
 -- Airport : Mechanized, mAA
 -- Airbase : Armored, Mechanized, lAA
 -- MilitaryBase : Armored, Mechanized, mAA
 -- Factory : Motorized, sAA
+-- Outpost : Infantry
